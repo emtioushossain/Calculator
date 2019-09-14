4bbaefc8b79b02203786f11d8479af3a97c133ec
emtioushossain <emtious16@gmail.com>
2019-09-14 13:36:29 +0600

    Creating & Connecting Disply Container : V-1

---
+++ src/container/display/display.js
@@ -0,0 +1,30 @@
+import React , {Component} from 'react'
+import { Container, Row, Col , Button } from 'reactstrap';
+import ReactDOM from 'react-dom';
+import {connect} from 'react-redux';
+import './display.css'
+
+class Display extends Component{
+  render(){
+    return(
+
+      <Row>
+          <Col md="12" className="displayBox">
+              <input className="inputBox" value={this.props.input_value} />
+              <input className="resultBox" value={this.props.calculation_result} />
+          </Col>
+      </Row>
+
+    )
+
+  }
+
+}
+
+
+const mapStateToProps = state => ({
+  calculation_result : state.final_result.calculated_value,
+  input_value : state.final_result.input_value
+})
+
+export default connect(mapStateToProps)(Display) ;
