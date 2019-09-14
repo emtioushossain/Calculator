f15a6f135a1ffca4e05be1a06ff8c5ef38ff02b1
emtioushossain <emtious16@gmail.com>
2019-09-14 11:41:42 +0600

    Creating actions part - action types , action functions : V-1

---
+++ src/actions/index.js
@@ -0,0 +1,44 @@
+import { FINAL_RESULT , CLICK_REMOVE_BUTTON , CLICK_INPUT_BUTTON , CLICK_CLEAR_BUTTON} from '../actions/actionTypes' ;
+
+
+export const calculateInput = (buttonValue) =>{
+  console.log("Final value")
+  console.log(buttonValue)
+  try{
+      buttonValue = eval(buttonValue)
+  }
+  catch(error){
+      buttonValue = ''
+  }
+
+  console.log(buttonValue);
+  return {
+    type : FINAL_RESULT ,
+    payload : buttonValue
+  }
+
+}
+
+export const receiveInput = (buttonValue) =>{
+  return {
+    type : CLICK_INPUT_BUTTON ,
+    payload : buttonValue
+  }
+
+}
+
+export const clearInput = (buttonValue) =>{
+  return {
+    type : CLICK_CLEAR_BUTTON ,
+    payload : buttonValue
+  }
+
+}
+
+export const clearLastInput = (buttonValue) =>{
+  return {
+    type : CLICK_REMOVE_BUTTON ,
+    payload : buttonValue
+  }
+
+}
